a9e51de492372c1b3a963ca44b66e70950f64c8b
Rodrigo Santos <rodrigo.silva@gcbinvestimentos.com>
2023-07-20 20:11:32 -0300

    ESLint configuration

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,12 +1,10 @@
-import { AnswerQuestionUseCase } from "./answer-question"
+import { AnswerQuestionUseCase } from './answer-question'
 
-import { AnswersRepository } from "../repositories/answers-repository"
-import { Answer } from "../entities/answer"
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
 
 const fakeAnswersRepository: AnswersRepository = {
-  create: async (answer: Answer) => {
-    return;
-  }
+  create: async (answer: Answer) => {},
 }
 
 test('create an answer', async () => {
